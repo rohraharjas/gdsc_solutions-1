73e74951d2b01f09029bef2c2bbb7a0b7cb8ccc6
HarjasRohra <C2K221269@ms.pict.edu>
2024-02-17 11:57:24 +0530

    created home page

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Home(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
